ca39fa03dfc6a7a9cd63dd1f3e8f80e931ba9a1e
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2020-09-11 04:47:06 -0400

    Create main.cpp

---
+++ classes/stores/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+class stores{
+private:
+	string name;
+	int rent;
+	double dueb;
+public:
+	void setname( string namee){
+		name=namee;
+	}
+	void setrent(int rentt){
+		rent= rentt;
+	}
+	void setdueb(double due){
+		dueb=due;
+	}
+	void display(){
+		cout<<"name of store is "<<name;
+		cout<< "\nmonthly rent of store is "<<rent;
+		cout<< " \ndue balance of store is "<<dueb;
+
+	}
+};
+int main()
+{
+	stores jcy;
+	jcy.setname("jcy");
+	jcy.setrent(550);
+	jcy.setdueb(250.2);
+	jcy.display();
+	return 0;
+}
