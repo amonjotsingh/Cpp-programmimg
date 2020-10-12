67452ef0b44d43a501371ff67a845f8da09665bf
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2020-10-12 06:13:27 -0400

    Update Main.cpp

--- classes/stores_constructors/Copy_constructor/Main.cpp
+++ classes/stores_constructors/Copy_constructor/Main.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 class stores{
+    
 private:
     string name;
     int rent;
     double dueb;
+    
 public:
     void setname( string namee){name=namee;}
     void setrent(int rentt){rent= rentt;}
@@ -22,9 +24,11 @@ public:
     stores(const stores&source);
     ~stores(){cout<<"destructor is called for :"<<name<<endl;}
 };
+
 stores::stores(const stores&source):name{source.name},rent{source.rent},dueb{source.dueb}{
     cout<<"copy constructor is called for store:"<<name<<endl;
 }
+
 stores::stores(string namee):stores{namee,0,0}{
     cout<<"one arg delegated const is called for:"<<name<<endl;
 }
@@ -32,14 +36,13 @@ stores::stores(string namee):stores{namee,0,0}{
 stores::stores(string namee,int rentt,double due):name{namee},rent{rentt},dueb{due}{
     cout<< "three arg constructor is called for : "<<name<<endl;
 }
+
 void display(stores s){
     cout<<"name of store is "<<s.getname();
     cout<< "\nmonthly rent of store is "<<s.getrent();
     cout<< " \ndue balance of store is "<<s.getdueb();
     cout<< "\n\n"<<endl;
-
 }
-
 
 int main()
 {
