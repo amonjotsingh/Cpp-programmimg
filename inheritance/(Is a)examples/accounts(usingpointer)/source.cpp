1f18c6aca1b8b5818899d9a66d34c376f6fed21f
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2020-11-12 05:07:00 -0500

    Create source.cpp

---
+++ inheritance/(Is a)examples/accounts(usingpointer)/source.cpp
@@ -0,0 +1,32 @@
+/*
+ * source.cpp
+ *
+ *  Created on: Oct 12, 2020
+ *      Author: amonjot
+ */
+
+#include"account.h"
+#include<iostream>
+#include"savingacc.h"
+
+void account::deposit(double amount){
+	balance =+ amount;
+	std::cout<<"amount added to "<<name<<"'s account"<<std::endl;
+	std::cout<<"new balance is "<<balance<<std::endl;
+}
+
+void account::withdraw(double amount){
+	std::cout<<" withdrawl from "<<name<<"'s account"<<std::endl;
+	if(balance>=amount)
+		balance =- amount;
+	else
+		std::cout<<"error: low balance"<<std::endl;
+}
+void savingacc::interestamount(double amount){
+	int months;
+	std::cout<<"how many months do you want to save your money for"<<std::endl;
+	std::cin>>months;
+	double interest;
+	interest= (amount*(0.8)*months/100);
+	std::cout<<"interest after "<<months<<" months would be "<< interest<<std::endl;
+}
