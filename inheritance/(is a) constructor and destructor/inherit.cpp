6b73c5fbd739c4d78486ca0908eeca4b5e79a930
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2020-11-19 05:46:54 -0500

    Create inherit.cpp

---
+++ inheritance/(is a) constructor and destructor/inherit.cpp
@@ -0,0 +1,15 @@
+// Name        : inherit.cpp
+// Author      : jot
+// Version     :
+// Copyright   : Your copyright notice
+
+#include <iostream>
+#include"account.h"
+#include"savingacc.h"
+using namespace std;
+
+int main() {
+	savingacc acc(0.5);
+
+	return 0;
+}
