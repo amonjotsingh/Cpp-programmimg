994049c7e1546d5c2fe9325b0dba78db4be7bc23
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2020-11-19 05:48:10 -0500

    Create source.cpp

---
+++ inheritance/(is a) constructor and destructor/source.cpp
@@ -0,0 +1,47 @@
+/*
+ * source.cpp
+ *
+ *  Created on: Oct 12, 2020
+ *      Author: amonjot
+ */
+
+#include"account.h"
+#include<iostream>
+#include"savingacc.h"
+
+account::account()
+{
+	name="blank";
+	balance=0;
+	std::cout<<"no args constructor is called"<<std::endl;
+
+}
+account::account(std::string nam,double amount)
+{
+	name=nam;balance=amount;
+	std::cout<<"name and balance cons is called "<<std::endl;
+}
+account::~account(){
+	std::cout<<"destructor for account class is called "<<std::endl;
+}
+
+void account::deposit(double amount){
+	balance =+ amount;
+	std::cout<<"amount added to "<<name<<"'s account"<<std::endl;
+	std::cout<<"new balance is "<<balance<<std::endl;
+}
+
+void account::withdraw(double amount){
+	std::cout<<" withdrawl from "<<name<<"'s account"<<std::endl;
+	if(balance>=amount)
+		balance =- amount;
+	else
+		std::cout<<"error: low balance"<<std::endl;
+}
+void savingacc::interestamount(int months){
+	std::cin>>months;
+	double interestam;
+	interestam= (balance*interest*months/100);
+	std::cout<<"interest amount after "<<months<<" months would be "<< interestam<<std::endl;
+}
+s
