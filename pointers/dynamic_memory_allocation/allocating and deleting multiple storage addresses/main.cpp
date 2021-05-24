63a600992cfae4aaa73c67be32bee94a41779e68
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2021-05-24 04:12:06 -0400

    Create main.cpp

---
+++ pointers/dynamic_memory_allocation/allocating and deleting multiple storage addresses/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * proj.cpp
+ *
+ *  Created on: May 18, 2021
+ *      Author: amonjot
+ */
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main(){
+
+	int *ptr{nullptr};
+	cout << ptr <<endl;
+	cout << "how much int storage is required ?"<< endl;
+	int storage(0);
+	cin >> storage;
+	ptr = new int[storage]; // allocating storage
+	cout << ptr <<endl; // printing first address of allocated memory
+
+	delete [] ptr;  // freeing allocated memory
+
+
+	return 0;
+}
