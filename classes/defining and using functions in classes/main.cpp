f440fa8afe0a7fb95231d7489b08098e7ee10f78
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2021-08-26 05:36:41 -0400

    Create main.cpp

---
+++ classes/defining and using functions in classes/main.cpp
@@ -0,0 +1,37 @@
+//============================================================================
+// Name        : Classes.cpp
+// Author      : Amonjot
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class stores{
+public:
+	string name{"BLANK"};
+	int rent{0};
+	void deposit(double money);
+	void talk(void){cout << "rent of the store is "<< rent <<endl;}
+};
+
+int main() {
+	stores jcy;
+	stores qq;
+	qq.rent = 500;
+	qq.name = "QQ";
+	qq.talk();
+
+	stores *opticals = new stores;
+
+
+	opticals->talk();
+
+	delete opticals;
+
+	return 0;
+}
